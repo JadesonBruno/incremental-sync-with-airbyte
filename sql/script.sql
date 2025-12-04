4a9ddff20f373103f295b0694641fe5c4520b396
JadesonBruno <jadesonbruno.a@outlook.com>
2025-12-04 10:17:24 -0300

    refactor: Indenting sql script

--- sql/script.sql
+++ sql/script.sql
@@ -2,7 +2,7 @@
 CREATE TABLE tb_customers(
   id integer PRIMARY KEY,
   name VARCHAR(200),
-  updated_at timestamp DEFAULT NOW() NOT NULL
+  updated_at timestamptz DEFAULT NOW() NOT NULL
 );
 
 -- Insert some records
@@ -15,11 +15,11 @@ SELECT * FROM tb_customers;
 
 -- Create function to update the 'updated_at' field on update
 CREATE OR REPLACE FUNCTION fn_set_timestamp()
-RETURNS TRIGGER AS $$
-    BEGIN
-        NEW.updated_at := NOW();
-        RETURN NEW;
-    END;
+    RETURNS TRIGGER AS $$
+        BEGIN
+            NEW.updated_at := NOW();
+            RETURN NEW;
+        END;
 $$ LANGUAGE plpgsql;
 
 -- Create trigger to call the function on update
